0dfaccdca6894a42a5fbf9ed921e9d4e47b05953
Rakibul Hasan Dihan <98779204+dihanrh@users.noreply.github.com>
2023-06-17 11:38:06 +0600

    Swap Number

---
+++ SwapNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SwapNumber
+{
+    class Program
+    {
+       // Write a C# Sharp program to swap two numbers.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two Number:");
+
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine("Before Swap , First Number : " + firstNumber + " and Second Number :" + secondNumber);
+
+            int tmp;
+            tmp = firstNumber; 
+            firstNumber= secondNumber;
+            secondNumber= tmp;
+
+            Console.WriteLine("After Swap , First Number : "+ firstNumber + " and Second Number :" + secondNumber ); 
+        }
+    }
+}
